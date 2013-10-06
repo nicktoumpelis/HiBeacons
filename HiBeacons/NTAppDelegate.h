2bdf7df27cdaa6e03c0e1f1db7000923128cbd48
Nick Toumpelis <nicktoumpelis@me.com>
2013-10-06 15:30:11 +0200

    Make initial commit

---
+++ HiBeacons/NTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NTAppDelegate.h
+//  HiBeacons
+//
+//  Created by Nick Toumpelis on 2013-10-06.
+//  Copyright (c) 2013 Nick Toumpelis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HiBeacons/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HiBeacons
+//
+//  Created by Nick Toumpelis on 2013-10-06.
+//  Copyright (c) 2013 Nick Toumpelis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NTAppDelegate class]));
+    }
+}
