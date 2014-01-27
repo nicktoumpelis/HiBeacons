8605c65de5a46bcc6cc6f9ee03118498efb6272b
Nick Toumpelis <nicktoumpelis@me.com>
2014-01-27 14:03:37 +0100

    Update licence info for HiBeaconsTests.m, the prefix file and main.m, and the copyright year across all files

--- HiBeacons/NTHiBeaconsDelegate.h
+++ HiBeacons/NTHiBeaconsDelegate.h
@@ -3,7 +3,7 @@
 //  HiBeacons
 //
 //  Created by Nick Toumpelis on 2013-10-06.
-//  Copyright (c) 2013 Nick Toumpelis.
+//  Copyright (c) 2013-2014 Nick Toumpelis.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
